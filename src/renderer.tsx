192d2e41122c210f10ca3ccfeebed0cbdc4cf749
hachibu <raymondsohn@gmail.com>
2020-07-18 13:22:19 -0700

    Add React

---
+++ src/renderer.tsx
@@ -0,0 +1,8 @@
+import './index.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Drag and drop your files!</div>,
+  document.getElementById('root')
+);
