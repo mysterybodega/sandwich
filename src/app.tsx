b4f62d65beee9752057719eb29faa3cf0ab4ff06
hachibu <raymondsohn@gmail.com>
2020-08-12 13:39:55 -0700

    Add version

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,15 @@ import './stylesheets/app.css'
 
 import React, { FC } from 'react'
 import SandwichDropzoneComponent from './components/sandwich-dropzone-component'
+import { remote } from 'electron'
 
 interface IAppProps {}
 
 const App: FC<IAppProps> = () => {
+  const appVersion = remote.app.getVersion()
   return (
     <div>
-      <h1>Sandwich Beta</h1>
+      <h1>Sandwich Beta (version {appVersion})</h1>
       <SandwichDropzoneComponent />
     </div>
   )
